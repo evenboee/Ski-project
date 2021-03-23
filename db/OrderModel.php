06b076400cd7040bbfa00b9bac78de779caf816d
Even Bryhn Bøe <evenbbo@stud.ntnu.no>
2021-03-23 12:44:29 +0100

    #6 - Added dbCredentialsTemplate.php

--- db/OrderModel.php
+++ db/OrderModel.php
@@ -11,7 +11,7 @@ class OrderModel extends DB {
     public function getOrdersWithState($state): array {
         $res = array();
 
-        $query = 'SELECT order_number FROM `order` WHERE state = :state';
+        $query = 'SELECT order_number FROM `ski_order` WHERE state = :state';
 
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':state', $state);
