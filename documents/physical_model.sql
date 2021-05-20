7840adbd3c8dda2af18ebcfc0a9e542148bcf55d
Amund Helgestad <amundhel@stud.ntnu.no>
2021-05-20 11:53:51 +0000

    added a single space

--- documents/physical_model.sql
+++ documents/physical_model.sql
@@ -126,7 +126,7 @@ CREATE TABLE `production_plan` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `start_date` DATE NOT NULL,
     `end_date` DATE NOT NULL,
-    `plannerNo`INT NOT NULL,
+    `plannerNo` INT NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
 
