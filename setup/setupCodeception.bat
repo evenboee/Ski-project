ffc62aa64aab3bae8847060d6300d4fa6cc2ccb5
Even Bryhn Bøe <evenbbo@stud.ntnu.no>
2021-05-20 10:44:41 +0000

    Added proper setup scrip

--- setup/setupCodeception.bat
+++
@@ -1,7 +0,0 @@
-Rem @author Rune Hjelsvold
-call composer require --dev "codeception/codeception"
-call composer require --dev "codeception/module-asserts"
-call composer require --dev "codeception/module-db"
-call composer require --dev "codeception/module-rest"
-call vendor\bin\codecept bootstrap
-call vendor\bin\codecept generate:suite api
